51ce6df2c24456f1619ba2a0ec99b0c4d769e3fd
Ayush Yadav <ayushyadav468@gmail.com>
2021-01-23 13:08:29 +0530

    node modules added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # personal-website
+
 This my personal website.
+
+For version 2.0, I am using tailwindcss.
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	theme: {
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		fontFamily: {
+			serif: ['Playfair Display', 'serif'],
+		},
+		colors: {
+			dark: '#211722',
+			red: '#B63B4F',
+			yellow: '#FCE205',
+			gray: '#798E9D',
+			light: '#F3F7F4',
+		},
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
