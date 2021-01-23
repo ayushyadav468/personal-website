fe9da7a50632028b4a26600ed978c82b50808bc4
Ayush Yadav <ayushyadav468@gmail.com>
2021-01-23 16:35:43 +0530

    main page created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,22 @@
 module.exports = {
 	theme: {
 		screens: {
-			sm: '480px',
-			md: '768px',
-			lg: '976px',
-			xl: '1440px',
+			sm: { max: '480px' },
+			md: { min: '481px', max: '768px' },
+			lg: { min: '769px', max: '976px' },
+			xl: { min: '977px', max: '1440px' },
 		},
 		fontFamily: {
-			serif: ['Playfair Display', 'serif'],
+			playFair: ['Playfair Display', 'serif'],
 		},
 		colors: {
+			dark: '#211722',
+			red: '#B63B4F',
+			yellow: '#FCE205',
+			gray: '#798E9D',
+			light: '#F3F7F4',
+		},
+		textColor: {
 			dark: '#211722',
 			red: '#B63B4F',
 			yellow: '#FCE205',
