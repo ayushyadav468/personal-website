5fd935817dc8ef02fa308339ea29f6f3e9ed96fe
Ayush Yadav <ayushyadav468@gmail.com>
2021-01-31 15:02:50 +0530

    tilt added to project cards

--- public/script.js
+++ public/script.js
@@ -1,55 +1,49 @@
-// const app = () => {
-// 	window.addEventListener('DOMContentLoaded', () => {
-// 		// get the form elements defined in your form HTML above
+const app = () => {
+	window.addEventListener('DOMContentLoaded', () => {
+		// get the form elements defined in your form HTML above
 
-// 		const form = document.querySelector('.contact-form');
+		const form = document.querySelector('.contact-form');
 
-// 		// Success and Error functions for after the form is submitted
+		// Success and Error functions for after the form is submitted
 
-// 		const success = () => {
-// 			form.reset();
-// 			window.location.pathname =
-// 				window.location.pathname + 'formSubmitted.html';
-// 			document.querySelector('.failed').style.display = 'none';
-// 		};
-// 		const error = () => {
-// 			window.location.pathname =
-// 				window.location.pathname + 'formSubmitted.html';
-// 			document.querySelector('.sucess').style.display = 'none';
-// 			document.querySelector('.failed').style.display = 'block';
-// 		};
+		const success = () => {
+			form.reset();
+			window.location.pathname =
+				window.location.pathname + 'formSubmitted.html';
+			document.querySelector('.failed').style.display = 'none';
+		};
+		const error = () => {
+			window.location.pathname =
+				window.location.pathname + 'formSubmitted.html';
+			document.querySelector('.sucess').style.display = 'none';
+			document.querySelector('.failed').style.display = 'block';
+		};
 
-// 		// handle the form submission event
+		// handle the form submission event
 
-// 		form.addEventListener('submit', function (ev) {
-// 			ev.preventDefault();
-// 			var data = new FormData(form);
-// 			ajax(form.method, form.action, data, success, error);
-// 		});
-// 	});
+		form.addEventListener('submit', function (ev) {
+			ev.preventDefault();
+			var data = new FormData(form);
+			ajax(form.method, form.action, data, success, error);
+		});
+	});
 
-// 	// helper function for sending an AJAX request
+	// helper function for sending an AJAX request
 
-// 	function ajax(method, url, data, success, error) {
-// 		var xhr = new XMLHttpRequest();
-// 		xhr.open(method, url);
-// 		xhr.setRequestHeader('Accept', 'application/json');
-// 		xhr.onreadystatechange = function () {
-// 			if (xhr.readyState !== XMLHttpRequest.DONE) return;
-// 			if (xhr.status === 200) {
-// 				success(xhr.response, xhr.responseType);
-// 			} else {
-// 				error(xhr.status, xhr.response, xhr.responseType);
-// 			}
-// 		};
-// 		xhr.send(data);
-// 	}
-// };
+	function ajax(method, url, data, success, error) {
+		var xhr = new XMLHttpRequest();
+		xhr.open(method, url);
+		xhr.setRequestHeader('Accept', 'application/json');
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState !== XMLHttpRequest.DONE) return;
+			if (xhr.status === 200) {
+				success(xhr.response, xhr.responseType);
+			} else {
+				error(xhr.status, xhr.response, xhr.responseType);
+			}
+		};
+		xhr.send(data);
+	}
+};
 
-// app();
-const svgLogo = document.querySelectorAll('.intro .name svg path');
-console.log(svgLogo);
-
-svgLogo.forEach((i) => {
-	console.log(`Path is ${i.getTotalLength()}`);
-});
+app();
