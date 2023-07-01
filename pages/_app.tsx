5b53f187a40569a6ed1464dad277e43fd2d73abe
Ayush Yadav <ayushyadav468@gmail.com>
2023-07-01 13:35:22 +0530

    package updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,18 @@
-import { Poppins } from '@next/font/google'
-import type { AppProps } from 'next/app'
-import '../styles/globals.css'
+import type { AppProps } from 'next/app';
+import { Poppins } from 'next/font/google';
+import '../styles/globals.css';
 
 const poppins = Poppins({
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  style: ['normal', 'italic'],
-  variable: '--font-poppins',
-})
+	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+	style: ['normal', 'italic'],
+	variable: '--font-poppins',
+	subsets: ['latin']
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <main className={`${poppins.variable} font-serif`}>
-      <Component {...pageProps} />
-    </main>
-  )
+	return (
+		<main className={`${poppins.variable} font-serif`}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
